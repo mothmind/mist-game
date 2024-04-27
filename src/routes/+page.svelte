0d1df64d1cc4861cc3a9f8adb820b4cb8f6a9550
mothmind <68579829+mothmind@users.noreply.github.com>
2024-04-27 08:47:06 -0600

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<main>
+  <h1>Welcome, Revenant</h1>
+  <p>
+    This is the concept page of a videogame. In this game, the player takes the
+    place of a Revenant Mariner, a dedicated explorer sailing through a deadly
+    ocean to discover new lands. It's meant to be a sort of Uncharted Waters
+    clone, but without the manual tile-based navigation.
+  </p>
+  <h2>Aetherhaven</h2>
+  <img
+    src="/city-concept.webp"
+    alt="A large fantastical metropolis filled with tall steel and glass spires and arcing multicolored lightning"
+  />
+  <p>
+    Aetherhaven, your home. Surrounded by a sea cloaked in perpetual mist, this
+    city is a beacon of advanced magical technologies and towering multicolored
+    skyscrapers. It pulses with the energy of arcing lightning and the hum of
+    arcane innovations, attracting adventurers, scholars, and mystics from all
+    corners of the world.
+  </p>
+
+  <h2>Drifter</h2>
+  <img src="/drifter.webp" alt="A small but cleaning looking explorer boat" />
+  <p>
+    The Drifter is a ship that the player uses to navigate the ocean. It's a
+    small, cheap ship that the player will want to replace as soon as possible,
+    but an upgraded ship will require more influence and crew to maintain.
+  </p>
+
+  <h2>Revenant Mariner</h2>
+  <img
+    src="/revenant-mariner.webp"
+    alt="An adventurous woman with a magical tattoo of a great sea beast"
+  />
+  <p>
+    You, the player. The player navigates this world with the aid of a
+    Revenant's Mark, a powerful magical tattoo that transports the wearer back
+    in time any time they die. Revenant Mariners chart and navigate The Lost
+    Haze by repeatedly dying and returning to the past. When the tattoo is
+    activated, the tattoo is updated with the cause of the Revenant's death, as
+    well as the last known direction the Revenant had set off in, and an updated
+    count of the number of times the tattoo has triggered.
+  </p>
+
+  <h2>The Lost Haze</h2>
+  <img
+    src="/lost-haze.webp"
+    alt="A vast and deadly ocean filled with monsters and storms"
+  />
+  <p>
+    The Lost Haze is a vast and deadly ocean filled with monsters, storms, and
+    other dangers. The player must navigate this ocean to discover new lands and
+    treasures, but the ocean is filled with dangers that can easily kill the
+    player. The player must use their wits and their ship to survive the dangers
+    of The Lost Haze and make it to the other side.
+  </p>
+</main>
+
+<style>
+  main {
+    width: 100%;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+</style>
